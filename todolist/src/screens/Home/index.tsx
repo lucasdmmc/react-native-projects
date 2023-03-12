2c7a3f1fc01951dcbd7f94ad2f3ee17ef8922a67
lucasdmmc <lucas.dmmc@hotmail.com>
2023-03-12 04:50:29 -0300

    Adicionei o logo da aplicação

---
+++ todolist/src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View, Image } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require("../../../assets/logo.png")} />
+    </View>
+  )
+}
---
+++ todolist/src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#0D0D0D",
+    flex: 1,
+    color: "red",
+    padding: 24,
+    alignItems: "center",
+  },
+  logo: {
+    marginTop: 25
+  }
+})
